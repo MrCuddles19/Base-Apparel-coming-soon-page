25d01d1a95c63a6f3fd80f38b88eae697b8ef03d
Mr.Cuddles19 <cecilamediku@gmail.com>
2024-07-19 06:13:52 -0700

    Complete Functionality with JavaScript

---
+++ script.js
@@ -0,0 +1,55 @@
+import { emails } from "./emails.js";
+
+const submitButton = document.querySelector('.submit-btn');
+const inputField = document.querySelector('.input-field');
+const inputSection = document.querySelector('.input-section');
+const errorIcon = document.querySelector('.error');
+const notificationRed = document.querySelector('.notification-red');
+const notificationGreen = document.querySelector('.notification-green');
+const loading = document.querySelector('.loading');
+
+inputField.addEventListener('input', () => {
+    const email = inputField.value;
+
+    // Reset states
+    errorIcon.classList.remove('visible');
+    notificationRed.classList.remove('appear');
+    inputSection.classList.remove('red');
+    notificationGreen.classList.remove('appear');
+
+    if (!validateEmail(email) && email !== '') {
+        errorIcon.classList.add('visible');
+        notificationRed.classList.add('appear');
+        inputSection.classList.add('red');
+    }
+});
+
+submitButton.addEventListener('click', () => {
+    const email = inputField.value;
+
+    if (emails.some(emailItem => emailItem.info === email)) {
+        alert('This e-mail has already been saved');
+    } else {
+        if (validateEmail(email)) {
+            loading.classList.add('appear');
+            setTimeout(() => {
+                loading.classList.remove('appear');
+                notificationGreen.classList.add('appear');
+                setTimeout(() => {
+                notificationGreen.classList.remove('appear');
+                },2000)
+            }, 3000);
+
+            emails.push({ info: email });
+        } else {
+            errorIcon.classList.add('visible');
+            notificationRed.classList.add('appear');
+            inputSection.classList.add('red');
+        }
+    }
+});
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
